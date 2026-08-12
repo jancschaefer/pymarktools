e2ac037c0411e58d670f1c840808b6c69ae5720f
Jan Schäfer (DE) <jan.schaefer@pwc.com>
2026-08-12 23:32:03 +0200

    fix: release correct package version

--- src/pymarktools/__init__.py
+++ src/pymarktools/__init__.py
@@ -1,11 +1,12 @@
 """pymarktools - A set of markdown utilities for Python."""
 
 import logging
+from importlib.metadata import version
 
 from .core.markdown import DeadImageChecker, DeadLinkChecker, ImageInfo, LinkInfo
 from .core.refactor import FileReference, FileReferenceManager
 
-__version__ = "0.3.0"
+__version__ = version("pymarktools")
 __all__ = [
     "DeadLinkChecker",
     "DeadImageChecker",
